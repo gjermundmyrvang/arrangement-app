4bf2000e3b7776c36298fa0ddd63b080bea7189b
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-04-28 22:24:21 +0200

    feat: add some test icons to homescreen from different families

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 import { View } from "react-native";
 import { Text } from "../components/ui";
+import { Icon } from "../components/ui/Icon";
 import { useTheme } from "../theme/ThemeProvider";
 
 export default function Index() {
@@ -11,6 +12,12 @@ export default function Index() {
     >
       <Text variant="title">Homescreen</Text>
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View className="flex flex-row items-center gap-2 mt-4">
+        <Text className="mr-2">Icons from different families:</Text>
+        <Icon family="Ionicons" name="home" />
+        <Icon family="Feather" name="settings" />
+        <Icon family="MaterialIcons" name="star" color={theme.titleText} />
+      </View>
     </View>
   );
 }
