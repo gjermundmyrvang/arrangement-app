eda6a821e89099319d53c50c25254fc7e01aa647
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-05-16 19:36:08 +0200

    feat: create a root navigator that based on session and profile status navigates user to correct stacks

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,40 @@
 import { Stack } from "expo-router";
+import { KeyboardProvider } from "react-native-keyboard-controller";
 import "../globals.css";
+import AuthProvider, { useAuth } from "../lib/AuthProvider";
 import SnackProvider from "../lib/SnackProvider";
 import { ThemeProvider } from "../theme/ThemeProvider";
 
+function RootNavigation() {
+  const { session, profile } = useAuth();
+
+  return (
+    <Stack>
+      <Stack.Protected guard={!!session && !!profile}>
+        <Stack.Screen name="(main)" options={{ headerShown: false }} />
+      </Stack.Protected>
+
+      <Stack.Protected guard={!session}>
+        <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+      </Stack.Protected>
+
+      <Stack.Protected guard={!!session && !profile}>
+        <Stack.Screen name="create-profile" options={{ headerShown: false }} />
+      </Stack.Protected>
+    </Stack>
+  );
+}
+
 export default function RootLayout() {
   return (
-    <ThemeProvider>
-      <SnackProvider>
-        <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-        </Stack>
-      </SnackProvider>
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        <SnackProvider>
+          <KeyboardProvider>
+            <RootNavigation />
+          </KeyboardProvider>
+        </SnackProvider>
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
